16ea58eead2f9b6946405179476e48ef235fe145
nt4rever <69197323+nt4rever@users.noreply.github.com>
2021-11-08 15:21:40 +0700

    learn knn

--- facerec_ipcamera_knn.py
+++ facerec_ipcamera_knn.py
@@ -1,38 +1,3 @@
-"""
-This is an example of using the k-nearest-neighbors (KNN) algorithm for face recognition.
-
-When should I use this example?
-This example is useful when you wish to recognize a large set of known people,
-and make a prediction for an unknown person in a feasible computation time.
-
-Algorithm Description:
-The knn classifier is first trained on a set of labeled (known) faces and can then predict the person
-in a live stream by finding the k most similar faces (images with closet face-features under euclidean distance)
-in its training set, and performing a majority vote (possibly weighted) on their label.
-
-For example, if k=3, and the three closest face images to the given image in the training set are one image of Biden
-and two images of Obama, The result would be 'Obama'.
-
-* This implementation uses a weighted vote, such that the votes of closer-neighbors are weighted more heavily.
-
-Usage:
-
-1. Prepare a set of images of the known people you want to recognize. Organize the images in a single directory
-   with a sub-directory for each known person.
-
-2. Then, call the 'train' function with the appropriate parameters. Make sure to pass in the 'model_save_path' if you
-   want to save the model to disk so you can re-use the model without having to re-train it.
-
-3. Call 'predict' and pass in your trained model to recognize the people in a live video stream.
-
-NOTE: This example requires scikit-learn, opencv and numpy to be installed! You can install it with pip:
-
-$ pip3 install scikit-learn
-$ pip3 install numpy
-$ pip3 install opencv-contrib-python
-
-"""
-
 import cv2
 import math
 from sklearn import neighbors
@@ -51,11 +16,6 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'JPG'}
 def train(train_dir, model_save_path=None, n_neighbors=None, knn_algo='ball_tree', verbose=False):
     """
     Trains a k-nearest neighbors classifier for face recognition.
-
-    :param train_dir: directory that contains a sub-directory for each known person, with its name.
-
-     (View in source code to see train_dir example tree structure)
-
      Structure:
         <train_dir>/
         ├── <person1>/
@@ -151,13 +111,7 @@ def predict(X_frame, knn_clf=None, model_path=None, distance_threshold=0.5):
 
 
 def show_prediction_labels_on_image(frame, predictions):
-    """
-    Shows the face recognition results visually.
-
-    :param frame: frame to show the predictions on
-    :param predictions: results of the predict function
-    :return opencv suited image to be fitting with cv2.imshow fucntion:
-    """
+    # Shows the face recognition results visually.
     pil_image = Image.fromarray(frame)
     draw = ImageDraw.Draw(pil_image)
 
